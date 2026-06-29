745ecd379db8cd7136c8cdd36cc886d933f632b0
Oskari Rauta <oskari.rauta@gmail.com>
2026-06-29 09:19:44 +0300

    luci-app-uxc: modernize
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- luci/luci-app-uxc/htdocs/luci-static/resources/view/status/include/23_uxc.js
+++ luci/luci-app-uxc/htdocs/luci-static/resources/view/status/include/23_uxc.js
@@ -18,17 +18,17 @@ return baseclass.extend({
 
 	render: function(containers) {
 
-		var table = E('table', { 'class': 'table', 'style': 'width=100%' });
+		var table = E('table', { 'class': 'table', 'style': 'width: 100%' });
 
 		if (!L.isObject(containers))
 			return E('div', { 'class': 'left', 'style': 'margin-bottom: 16px;' }, _('Error while retrieving list of containers'));
 
 		table.appendChild(E('tr', { 'class': 'tr' }, [
-			E('td',  { 'class': 'td left', 'style' : 'width: 20%' }, '<b>' + _('Name') + '</b>'),
-			E('td',  { 'class': 'td left', 'style': 'width: 20%;' }, '<b>' + _('Bundle') + '</b>'),
-			E('td',  { 'class': 'td center', 'style': 'width: 10;' }, '<b>' + _('PID') + '</b>'),
-			E('td',  { 'class': 'td left', 'style': 'width: 100%;' }, '&nbsp;'),
-			E('td',  { 'class': 'td left', 'style': 'width: 15%;'  }, '<b>' + _('Running') + '</b>')
+			E('td', { 'class': 'td left', 'style': 'width: 20%;' }, '<b>' + _('Name') + '</b>'),
+			E('td', { 'class': 'td left', 'style': 'width: 20%;' }, '<b>' + _('Bundle') + '</b>'),
+			E('td', { 'class': 'td center', 'style': 'width: 10%;' }, '<b>' + _('PID') + '</b>'),
+			E('td', { 'class': 'td left', 'style': 'width: 100%;' }, '&nbsp;'),
+			E('td', { 'class': 'td left', 'style': 'width: 15%;' }, '<b>' + _('Running') + '</b>')
 		]));
 
 		var cnt = 0;
